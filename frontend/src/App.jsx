6aad4c6122acb5d6d802b1df31832a7e47dd7c63
Uvyne <ropchiri7@gmail.com>
2025-01-23 00:03:28 +0300

    added handle submit function

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import {React, useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  const [firstName, setFirstName] = useState("");
+  const [ lastName, setLastName ] = useState("");
+  const [ email, setEmail ] = useState("");
+  const[ contact, setContact ] = useState("");
+  const [gender, setGender ] = useState("male");
+  const [subjects, setSubjects ] = useState({
+    english: true,
+     maths: false,
+    physics: false,
+  });
+  const[resume, setResume] = useState("");
+  const[url, setUrl] = useState("");
+  const [ selectedOption, setSelectedOption] = useState("");
+  const [about, setAbout] = useState("");
+  const handleSubmit = (e) =>{
+    e.preventDefault();
+    console.log(
+      firstName,
+      lastName,
+      email,
+      contact,
+      selectedOption,
+      subjects,
+      resume,
+      url,
+      about
+    );
+  };
 }
-
-export default App
